cd086aee9209b1d4d39c5b407853c4a24d8d0a70
Chalizi <Chalizi@qq.com>
2019-09-03 18:09:33 +0800

    增加添加记录

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,152 @@
 <template>
   <div id="app">
-    <h1>网咖账单打印辅助程序</h1>
-    <el-row>
-      <el-table
-        :data="tableData"
-        style="width: 100%">
-        <el-table-column
-          prop="records"
-          label="消费记录"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="num"
-          label="数量"
-          width="180">
-        </el-table-column>
-        <el-table-column
-          prop="price"
-          label="单价"
-          width="180"
-        >
-        </el-table-column>
-        <el-table-column
-          prop="totalprice"
-          label="金额">
-        </el-table-column>
-      </el-table>
-    </el-row>
-    <el-row>
-      <el-button @click="print" type="primary" icon="el-icon-printer"></el-button>
-    </el-row>
-    <div>{{totalprice}}</div>
+    <div id="container">
+      <h1>账单打印辅助程序</h1>
+      <el-row>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="records"
+            label="消费记录"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="num"
+            label="数量"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="price"
+            label="单价"
+            width="180"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="totalprice"
+            label="金额">
+          </el-table-column>
+        </el-table>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          <div>消费记录</div>
+        </el-col>
+        <el-col :span="3">
+          <el-cascader
+            clearable
+            filterable
+            placeholder="试试搜索：指南"
+            v-model="records"
+            @change="choiceRecords(records)"
+            :options="options"
+            :show-all-levels="false"
+            :props="{ expandTrigger: 'hover', emitPath: false }"
+          ></el-cascader>
+        </el-col>
+        <el-col :span="3">
+          <div>数量</div>
+        </el-col>
+        <el-col :span="6">
+          <el-input-number
+            :disabled="numDisabled"
+            v-model="num"
+            :min="1"
+            :max="99"
+            label="数量"
+          ></el-input-number>
+        </el-col>
+        <el-col :span="3">
+          <div>{{priceName}}</div>
+        </el-col>
+        <el-col :span="3">
+          <el-input
+            placeholder="请输入内容"
+            v-model="price"
+            clearable>
+          </el-input>
+        </el-col>
+        <el-col :span="3">
+          <el-button
+            type="primary"
+            @click="add"
+          >添加</el-button>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="21" :span="3">
+          <el-button
+            @click="print"
+            type="primary"
+            icon="el-icon-printer"
+          ></el-button>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
+import options from './options.js'
+import menus from './menus.js'
 
 export default {
   name: 'App',
   data () {
     return {
+      numDisabled: false,
+      priceName: '单价',
+      records: '',
+      num: 1,
+      price: 20,
+      menus: menus,
       tableData: [
         {
-          records: '2016-05-02',
+          records: '口味王',
           num: 1,
-          price: 5
+          price: 20,
+          totalprice: 20
         }
-      ]
+      ],
+      options: options
     }
   },
   methods: {
     print () {
       window.print()
-    }
-  },
-  computed: {
-    totalprice () {
-      return this.tableData.num * this.tableData.price
+    },
+    add () {
+      var records = this.records
+      var num = this.num
+      var price = this.price
+      var totalprice = num * price
+      if (!records) {
+        this.$message({
+          message: '请先添加记录',
+          type: 'warning'
+        })
+        return
+      }
+      this.tableData.push({
+        records: records,
+        num: num,
+        price: price,
+        totalprice: totalprice
+      })
+      this.$message({
+        message: '添加成功',
+        type: 'success'
+      })
+    },
+    choiceRecords (records) {
+      console.log(records)
+      if (records === '充值') {
+        this.num = 1
+        this.priceName = '金额'
+        this.numDisabled = true
+      } else {
+        this.numDisabled = false
+      }
     }
   }
 }
@@ -70,5 +160,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#container {
+  width: 800px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/options.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    value: '饮品',
+    label: '饮品',
+    children: [
+      {
+        value: '奶茶',
+        label: '奶茶'
+      }
+    ]
+  },
+  {
+    value: '主食',
+    label: '主食',
+    children: [
+      {
+        value: '盖浇饭',
+        label: '盖浇饭',
+        children: [
+          {
+            value: '豆角茄子',
+            label: '豆角茄子'
+          },
+          {
+            value: '万载剁肉',
+            label: '万载剁肉'
+          },
+          {
+            value: '小炒鸡蛋',
+            label: '小炒鸡蛋'
+          }
+        ]
+      },
+      {
+        value: '排骨',
+        label: '排骨',
+        children: [
+          {
+            value: '麻辣翅中',
+            label: '麻辣翅中'
+          },
+          {
+            value: '香辣排骨',
+            label: '香辣排骨'
+          },
+          {
+            value: '绝味鸭脖',
+            label: '绝味鸭脖'
+          }
+        ]
+      },
+      {
+        value: '烧烤',
+        label: '烧烤',
+        children: [
+          {
+            value: '烤肉串',
+            label: '烤肉串'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    value: '百货',
+    label: '百货',
+    children: [
+      {
+        value: '打火机',
+        label: '打火机'
+      },
+      {
+        value: '纸巾',
+        label: '纸巾'
+      },
+      {
+        value: '扑克',
+        label: '扑克'
+      }
+    ]
+  },
+  {
+    value: '充值',
+    label: '充值'
+  }
+]
